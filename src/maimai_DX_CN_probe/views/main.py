94cedf97964d37111c82d01f0c9d77fdc05bfb2e
yuangezhizao-workaccount <chenyuan.gao@alsi.cn>
2020-10-21 16:12:39 +0800

    ✨ Feat(home.html): maimai 路由

--- src/maimai_DX_CN_probe/views/main.py
+++ src/maimai_DX_CN_probe/views/main.py
@@ -7,6 +7,7 @@
     :Copyright: © 2020 yuangezhizao <root@yuangezhizao.cn>
 """
 import flask
+from maimai_DX_CN_probe.models.maimai import HOME
 
 app = flask.current_app
 bp = flask.Blueprint('main', __name__)
@@ -15,3 +16,22 @@ bp = flask.Blueprint('main', __name__)
 @bp.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@bp.route('/maimai')
+def maimai():
+    if 'home' in flask.request.args:
+        HOME_data = HOME.query.order_by(HOME.id.desc()).all()
+
+        id_data, rating_data, rating_max_data, star_data, cache_dt_data = [], [], [], [], []
+        for data in HOME_data:
+            id_data.append(data.id)
+            rating_data.append(data.rating / 100)
+            rating_max_data.append(data.rating_max / 100)
+            star_data.append(data.star)
+            cache_dt_data.append(data.cache_dt.strftime('%Y-%m-%d %H:%M:%S'))
+
+        return flask.render_template('maimai/home.html', HOME_data=HOME_data, id_data=id_data, rating_data=rating_data,
+                                     rating_max_data=rating_max_data, star_data=star_data, cache_dt_data=cache_dt_data)
+    else:
+        return flask.redirect(flask.url_for('main.maimai', _external=True) + '?home')
