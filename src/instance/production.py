afd119908c10a61dc5efe799884e4d1d8a327e6c
yuangezhizao-workaccount <chenyuan.gao@alsi.cn>
2020-10-22 10:27:17 +0800

    🙈 See-no-evil(.gitignore): 取消排除 Flask 实例文件夹

---
+++ src/instance/production.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env/ python3
+# -*- coding: utf-8 -*-
+"""
+    :Author: yuangezhizao-workaccount
+    :Time: 2020/10/21 15:40
+    :Site: https://www.yuangezhizao.cn
+    :Copyright: © 2020 yuangezhizao <root@yuangezhizao.cn>
+"""
+
+SQLALCHEMY_BINDS = {
+    'maimai': 'mysql+pymysql://<rm>:<rm>@10.0.2.11/maimai',
+}
