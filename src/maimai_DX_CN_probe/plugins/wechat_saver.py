07152e66dd37e712a9dc57f40fb4b93b439b7aa0
yuangezhizao-MacMini <yuangezhizao@icloud.com>
2020-12-13 01:55:54 +0800

    ✨ Feat(wechat_saver.py): 实装 save_record(raw_html)

--- src/maimai_DX_CN_probe/plugins/wechat_saver.py
+++ src/maimai_DX_CN_probe/plugins/wechat_saver.py
@@ -10,7 +10,7 @@ import datetime
 
 from lxml import etree
 
-from maimai_DX_CN_probe.models.maimai import HOME, PlayerData, album
+from maimai_DX_CN_probe.models.maimai import HOME, PlayerData, album, Record
 
 
 def save_home(raw_html):
@@ -86,3 +86,73 @@ def save_playerData_album(raw_html):
             new_count += 1
 
     return [new_count, album_count]
+
+
+def save_record(raw_html):
+    selector = etree.HTML(raw_html)
+
+    new_count = 0
+    record_count = len(selector.xpath('//div[@class="p_10 t_l f_0 v_b"]'))
+
+    for i in range(record_count + 1, 1, -1):
+        level_img_s = selector.xpath(f'/html/body/div[2]/div[{i}]/div[1]/img/@src')[0].split('/')[-1].split('.')[0]
+        try:
+            vs_img_s = selector.xpath(f'/html/body/div[2]/div[{i}]/div[1]/img[3]/@src')[0] \
+                .split('/')[-1].split('.')[0]
+        except Exception as e:
+            vs_img_s = None
+
+        track = selector.xpath(f'/html/body/div[2]/div[{i}]/div[1]/div[1]/span[1]/text()')[0].split(' ')[1]
+        play_dt = selector.xpath(f'/html/body/div[2]/div[{i}]/div[1]/div[1]/span[2]/text()')[0]
+
+        play_dt_utc = datetime.datetime.strptime(play_dt, '%Y/%m/%d %H:%M') - datetime.timedelta(hours=8)
+        # play_ts_utc = int(time.mktime(play_dt_utc.timetuple()))
+
+        try:
+            selector.xpath(f'/html/body/div[2]/div[{i}]/div[2]/div[1]/img/@src')[0].split('/')[-1].split('.')[0]
+            clear = True
+        except Exception as e:
+            clear = False
+        name = selector.xpath(f'/html/body/div[2]/div[{i}]/div[2]/div[1]/text()')[0]
+        img_s = selector.xpath(f'/html/body/div[2]/div[{i}]/div[2]/div[2]/img[1]/@src')[0]
+        dx_img_s = selector.xpath(f'/html/body/div[2]/div[{i}]/div[2]/div[2]/img[2]/@src')[0] \
+            .split('/')[-1].split('.')[0]
+        achievement_0 = selector.xpath(f'/html/body/div[2]/div[{i}]/div[2]/div[2]/div/div[2]/text()')[0]
+        achievement_1 = selector.xpath(f'/html/body/div[2]/div[{i}]/div[2]/div[2]/div/div[2]/span/text()')[0]
+        achievement = int(achievement_0) + float(f'0{achievement_1}'[:-1])
+        try:
+            selector.xpath(f'/html/body/div[2]/div[{i}]/div[2]/div[2]/div/img[3]/@src')[0]
+            score_rank_new = True
+            score_rank_img_s = selector.xpath(f'/html/body/div[2]/div[{i}]/div[2]/div[2]/div/img[2]/@src')[0] \
+                .split('/')[-1].split('.')[0]
+        except Exception as e:
+            score_rank_new = False
+            score_rank_img_s = selector.xpath(f'/html/body/div[2]/div[{i}]/div[2]/div[2]/div/img[1]/@src')[0] \
+                .split('/')[-1].split('.')[0]
+        delux_score = selector.xpath(f'/html/body/div[2]/div[{i}]/div[2]/div[2]/div/div[3]/div[1]/div/text()')[0] \
+            .split('/')[-1].split('.')[0].replace(',', '')
+        try:
+            selector.xpath(f'/html/body/div[2]/div[{i}]/div[2]/div[2]/div/div[3]/div[1]/img[2]/@src')[0]
+            delux_new = True
+        except Exception as e:
+            delux_new = False
+        fc_img_s = selector.xpath(f'/html/body/div[2]/div[{i}]/div[2]/div[2]/div/div[3]/img[1]/@src')[0] \
+            .split('/')[-1].split('.')[0]
+        fs_img_s = selector.xpath(f'/html/body/div[2]/div[{i}]/div[2]/div[2]/div/div[3]/img[2]/@src')[0] \
+            .split('/')[-1].split('.')[0]
+        try:
+            rate_img_s = selector.xpath(f'/html/body/div[2]/div[{i}]/div[2]/div[2]/div/div[3]/img[3]/@src')[0] \
+                .split('/')[-1].split('.')[0]
+        except Exception as e:
+            rate_img_s = None
+        cache_dt = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+
+        if not Record.query.filter(Record.play_dt == play_dt).first():
+            new_maimai_Record = Record(level_img_s, vs_img_s, track, play_dt, play_dt_utc, clear, name, img_s, dx_img_s,
+                                       achievement,
+                                       score_rank_new, score_rank_img_s, delux_score, delux_new, fc_img_s, fs_img_s,
+                                       rate_img_s, cache_dt)
+            new_maimai_Record.save()
+            new_count += 1
+
+    return [new_count, record_count]
--- src/maimai_DX_CN_probe/views/main.py
+++ src/maimai_DX_CN_probe/views/main.py
@@ -9,7 +9,7 @@
 import flask
 
 from maimai_DX_CN_probe.models.maimai import HOME, PlayerData, album, Record, playlogDetail, musicInfo, practice
-from maimai_DX_CN_probe.plugins.wechat_saver import save_home, save_playerData, save_playerData_album
+from maimai_DX_CN_probe.plugins.wechat_saver import save_home, save_playerData, save_playerData_album, save_record
 
 app = flask.current_app
 bp = flask.Blueprint('main', __name__)
@@ -137,6 +137,9 @@ def wechat_saver():
         elif html_type == 'playerData_album':
             r = save_playerData_album(raw_html)
             flask.flash(f'[相册] 存储成功：{r}', 'success')
+        elif html_type == 'record':
+            r = save_record(raw_html)
+            flask.flash(f'[游戏记录] 存储成功：{r}', 'success')
         else:
             flask.flash(f'非法 [html_type]', 'negative')
     return flask.render_template('maimai/wechat_saver/manual.html')
