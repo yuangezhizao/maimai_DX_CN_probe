382267ff2fd76c8bb4740ef0c0dbd13c711aac2e
yuangezhizao-MacMini <yuangezhizao@icloud.com>
2021-08-13 01:47:24 +0800

    🔧 Config(/src/maimai_dx_cn_probe/__init__.py): 配置文件格式切换：py → yaml

--- src/maimai_DX_CN_probe/__init__.py
+++ src/maimai_DX_CN_probe/__init__.py
@@ -10,6 +10,7 @@ import os
 import time
 
 import flask
+import yaml
 
 from config import config
 
@@ -36,7 +37,7 @@ def create_app(config_name=None):
     app.wsgi_app = ReverseProxied(app.wsgi_app)
 
     app.config.from_object(config[config_name])
-    app.config.from_pyfile(f'{config_name}.py')
+    app.config.from_file(f'{config_name}.yaml', load=yaml.safe_load)
 
     register_extensions(app)
     register_blueprints(app)
